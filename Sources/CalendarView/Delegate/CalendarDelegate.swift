9299b0e2cff90aaafe10d91dc5c07550f1cf6494
ArashML <A.Mirilavasani@Alibaba.ir>
2022-02-27 14:52:07 +0330

    update delegate method
    move all different methods to single protocol

--- Sources/CalendarView/Delegate/CalendarDelegate.swift
+++ Sources/CalendarView/Delegate/CalendarDelegate.swift
@@ -2,12 +2,7 @@ import Foundation
 
 public protocol CalendarDelegate: AnyObject {
     func infoText(date: Date) -> String
-}
-
-public protocol CalendarSingleSelectionDelegate: AnyObject {
+    func failedToSelectDate(error: Error)
     func didSelect(date: Date)
-}
-
-public protocol CalendarRangeSelectionDelegate: AnyObject {
     func didSelectDateRange(lowerBound: Date, upperBound: Date?)
 }
