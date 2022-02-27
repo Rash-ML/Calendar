aa94f58e0be0727e341426f4474e050639e48126
ArashML <A.Mirilavasani@Alibaba.ir>
2022-02-27 11:58:26 +0330

    edit package structure
    add resource folder

--- Package.swift
+++ Package.swift
@@ -9,7 +9,9 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "CalendarView",
-            targets: ["CalendarView"]),
+            targets: [
+                "CalendarView",
+            ]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +22,15 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CalendarView",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources"),
+            ]
+        ),
         .testTarget(
             name: "CalendarViewTests",
-            dependencies: ["CalendarView"]),
+            dependencies: [
+                "CalendarView",
+            ]),
     ]
 )
